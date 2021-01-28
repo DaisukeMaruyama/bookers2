2bc69df02b494e47241807242f60eb65675abf79
DaisukeMaruyma <maruyama.d8@gmail.com>
2021-01-28 04:35:44 +0000

    Update files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+
   root to: 'home#top'
   get 'about' => 'home#about'
-  get '/' => "home#top"
 
   devise_for :users
 
+  resources :users, only: [:show, :index, :update, :edit]
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
